e5c1ad06566c82dc3bd058004a430b567ca8a6b7
SashaBohdanov <oleksandr.bohdanov.ik.2023@lpnu.ua>
2023-10-06 10:12:07 +0300

    first master commit

---
+++ PR3.4/PR3.4.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	double x; // вхідний аргумент	
+	double y; // вхідний параметр
+	double R;
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+	cout << "R = "; cin >> R;
+
+	// розгалуження в повній формі
+	if ((y*y-2*y*R+R<=-x*x-2*x*R) && y>=-R && -R<=x<=0 ||
+		(y*y+2*y*R>=-x*x+2*x*R && y<=R && 0<x<=R))
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+	cin.get();
+	return 0;
+}
